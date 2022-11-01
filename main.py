b8b071bba777b89f31398f0a49f5546b090141eb
Lasantha Mudalige <lasantha1130@gmail.com>
2022-11-01 19:06:53 +0530

    complete program

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-import webbrowser
 from PIL import ImageGrab
 import pyautogui
 from time import sleep
 
 
 def main():
-    # Go to the related website to play dinosaur game
-    webbrowser.open_new_tab("https://elgoog.im/t-rex/")
     # wait for 5 seconds to load it
     sleep(5)
     # Start the game by pressing space
-    pyautogui.press("space")
+    pyautogui.press("up")
 
     on = True
     while on:
         # Get full screen as an image
-        screen = ImageGrab.grab(xdisplay="")
+        screen = ImageGrab.grab()
 
-        # Few pixels in front of dinosaur's nose
-        mouth_pixel_color = screen.getpixel(xy=(1342, 471))
         # Few pixels in front of dinosaur's hands
-        hands_pixel_color = screen.getpixel(xy=(1337, 479))
+        hands_pixel_color1 = screen.getpixel(xy=(1310, 462))
+        hands_pixel_color2 = screen.getpixel(xy=(1310, 465))
+        hands_pixel_color3 = screen.getpixel(xy=(1310, 467))
+        hands_pixel_color4 = screen.getpixel(xy=(1310, 469))
+        hands_pixel_color5 = screen.getpixel(xy=(1310, 472))
+        hands_pixel_color4 = screen.getpixel(xy=(1310, 475))
 
-        # If mouth pixel or hands pixels turn to black press space using pyautogui
-        if mouth_pixel_color == (83, 83, 83) or hands_pixel_color == (83, 83, 83):
-            pyautogui.press("space")
-
-            if mouth_pixel_color == (83, 83, 83):
-                print(f"mouth pixel {mouth_pixel_color}")
-            elif hands_pixel_color == (83, 83, 83):
-                print(f"hands pixel {hands_pixel_color}")
+        if hands_pixel_color1 == (83, 83, 83) or hands_pixel_color2 == (83, 83, 83) or hands_pixel_color3 == (83, 83, 83) or hands_pixel_color4 == (83, 83, 83):
+            pyautogui.press("up")
 
 
 main()
